da33d41577e5fba5bc50aecc1d9b29562e58e7d1
ecmadao <wlec@outlook.com>
2016-10-05 01:11:47 +0800

    style

--- app/frontend/javascripts/components/UserComponent/User/Cliper/CliperContent.jsx
+++ app/frontend/javascripts/components/UserComponent/User/Cliper/CliperContent.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
 import CliperOperation from './CliperOperation';
 
 class CliperContent extends React.Component {
   render() {
     const {cliper} = this.props;
+    const containerClass = classNames('content_container', {
+      'loved': cliper.love
+    });
     return (
-      <div className="content_container">
+      <div className={containerClass}>
         {cliper.content}
         <CliperOperation
           id={cliper.id}
